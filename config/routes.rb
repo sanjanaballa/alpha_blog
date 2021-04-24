165c2f605feb32ccb3a2887a22d53838a7d4aac6
sanjanaballa <sanjana.balla@gmail.com>
2021-04-24 12:11:51 -0700

    create new template, ation, and route

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#home'
   get 'about', to: 'pages#about'
+
+  resources :articles
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
 end
